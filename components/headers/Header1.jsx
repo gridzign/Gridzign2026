260358bda3ed9de476054ef76644bbd535a46c3b
Yaseen Mirshal <yaseenmirshal5@gmail.com>
2026-01-14 19:04:37 +0530

    first commit

---
+++ components/headers/Header1.jsx
@@ -0,0 +1,59 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import AnimatedButton from "../animation/AnimatedButton";
+import { usePathname } from "next/navigation";
+import ThemeSwitcherButton from "./ColorSwitcher";
+
+export default function Header1() {
+  const pathname = usePathname();
+  const [isHidden, setIsHidden] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setIsHidden(currentScrollPos > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header id="header" className={`mxd-header ${isHidden ? "is-hidden" : ""}`}>
+      {/* header logo */}
+      <div className="mxd-header__logo loading__fade">
+        <Link href={`/home-main`} className="mxd-logo">
+          {/* logo icon */}
+          {/* <Image src="/public/img/icons/gridzign logo crop.png" alt="GridZign" width={100} height={100} /> */}
+          {/* logo text */}
+          <span className="mxd-logo__text">
+            GridZign
+          </span>
+        </Link>
+      </div>
+      {/* header controls */}
+      <div className="mxd-header__controls loading__fade">
+        <ThemeSwitcherButton />
+
+        {pathname == "/" || pathname == "/preview" ? (
+          <AnimatedButton
+            text="Purchase"
+            className="btn btn-anim btn-default btn-mobile-icon btn-outline slide-right"
+            href="https://themeforest.net/user/ib-themes/portfolio"
+            target="_blank"
+          >
+            <i className="ph-bold ph-shopping-cart-simple" />
+          </AnimatedButton>
+        ) : (
+          <AnimatedButton
+            text="Say Hello"
+            className="btn btn-anim btn-default btn-mobile-icon btn-outline slide-right"
+            href="/contact"
+          >
+            <i className="ph-bold ph-arrow-up-right" />
+          </AnimatedButton>
+        )}
+      </div>
+    </header>
+  );
+}
